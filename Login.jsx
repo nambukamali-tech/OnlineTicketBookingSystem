6a36ef6ea0af3168b70bdd6f45d4257bcb5a600d
Nambu Kamali <nambukamali@gmail.com>
2025-11-07 22:53:36 +0530

    Add files via upload

---
+++ Login.jsx
@@ -0,0 +1,27 @@
+import react from "react";
+import "./Login.css";
+function Login()
+{
+    return(   
+        <div className="login-container">
+            <div className="login-box">
+                <h1>Login</h1>
+                <p>Welcome to the Ticketry! Please Login to enter the Travel World!</p>
+                <form className="login-form">
+                    <div className="login-group">
+                        <label>Email</label>
+                        <input type="email" placeholder="Enter your Email ID" required/>
+                    </div>
+                    <div className="login-group">
+                        <label>Password</label>
+                        <input type="password" placeholder="Enter your Email Password" required/>
+                    </div>
+                    <button className="login-button">LOGIN</button>
+                    <p className="signup-link">Don't have an account!Please<a href="Signup">SIGNUP</a></p>
+                </form>
+            </div>
+        </div>
+
+    );
+}
+export default Login;
