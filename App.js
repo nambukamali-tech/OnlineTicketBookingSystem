dbe575c0690fd9a2d2717d9ca3f43bff4140f09f
Nambu Kamali <nambukamali@gmail.com>
2025-11-09 08:31:13 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 // Create router using new v7 API
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
+  {path: "/Login", element: <Login />},
+  {path: "/Signup", element: <Signup />},
 ]);
 function App()
 {
  return <RouterProvider router={router} />;
 }
 export default App;
+
+
 
 
