04fb45018e5e0b749214a021b5b0f8ad48dd83f5
Nambu Kamali <nambukamali@gmail.com>
2025-11-11 22:48:05 +0530

    Add files via upload

---
+++ Booking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTicketBookingSystem.Models
+{
+    public class Booking
+    {
+        [Required(ErrorMessage ="Travel ID is Mandatory")]
+        public int TravelId { get; set; }
+        [Required(ErrorMessage ="Departure Location is required")]
+        public string From { get; set; }
+        [Required(ErrorMessage ="Destination Location is required")]
+        public string To { get; set; }
+        [Required(ErrorMessage ="Members should be between 1 and 10")]
+        public int Members { get; set; }
+        [Required(ErrorMessage ="Price must be greater than 0")]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage ="Travel Date is required")]
+        public DateTime TravelDate { get; set; }
+    }
+}
